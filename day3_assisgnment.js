e3221bb4cfaad0527a8bfc77e71a1bba2163cb7d
Zahra11Mosbal <zahramosbal@gmail.com>
2025-12-04 01:36:11 +0300

    day 3 assignment

---
+++ day3_assisgnment.js
@@ -0,0 +1,22 @@
+// Day 3 Assignment part 1
+
+let procurementTongue = 500
+console.log("is the procurementTongue 1000kg: " , procurementTongue >=1000)
+
+let paymentStr = "5000"
+let paymentNum = 5000
+
+console.log("is the paymentStr equal to paymentNum: " , paymentStr === paymentNum)
+
+// Day 3 Assignment part 2
+
+let dealerName = "Zahra suliman"
+let costUGX = 2500
+
+console.log("is the length of dealerName more than or equal 2 and costUGX more than 100000: " ,
+ dealerName.length >=2 && costUGX > 1000000)
+
+ let saleDate = new Date("2025-11-3")
+
+ console.log("sale on :", saleDate.getDate(),"/", 
+ saleDate.getMonth()+1,"/", saleDate.getFullYear())
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-console.log("hi im ")
-let val = "33"
-console.log(val+3)
-console.log(Number(val)+3)
-let first = "you are doing"
 
-let cap = `${first.replace("you", "me")}`
-console.log(cap)
+let scol1 = 1333
+let scol2 = 333
+
+let chek = scol1 <= scol2
+console.log(chek)
+console.log("Hello World!")
+let pastday = new Date("2025-011-15")
+let curntday = new Date()
+let timediff = pastday.getTime() - curntday.getTime()
+
+console.log(-(timediff / 1000 / 60 / 60 / 24) + " days left until June 15, 2025")
+
